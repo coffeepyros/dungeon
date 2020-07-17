b7d4f40723f46c40fcc1a1ac4548bc5f344d7ad1
coffeepyros <coffeepyros@gmail.com>
2020-07-17 15:12:35 +0200

    bonk!

--- dungeon.js
+++ dungeon.js
@@ -6,15 +6,22 @@ const player = {
   posX: 4,
   posY: 4,
 };
+const monster = {
+  posX: 5,
+  posY: 5,
+};
 
 function drawMap() {
   for (let CellY = 1; CellY <= MapSizeY; CellY++) {
     for (let CellX = 1; CellX <= MapSizeX; CellX++) {
       let output = `<figure class="cell floor_stone" title="x:${CellX},y:${CellY}">`;
       // If the map cell has the player in it,
-      if (player.posX == CellX && player.posY == CellY) {
+      if (monster.posX == CellX && monster.posY == CellY) {
         // draw a player symbol
-        output += "P";
+        output += "M";
+      } else if (player.posX == CellX && player.posY == CellY) {
+        // draw a player symbol
+        output += '<span id="player">P</span>';
       }
       //    else if (
       //     // Surrounding the player, with the distance of 1
@@ -30,14 +37,27 @@ function drawMap() {
   }
 }
 
-// Moves the player to a new position, specified by (x,y)
-function go(NewX, NewY) {
-  player.posX = NewX;
-  player.posY = NewY;
+function reDraw() {
   // Emptying the Map before redrawing
   map.innerHTML = "";
   // Redrawing the Map with the new player position
   drawMap();
+}
+
+// Moves the player to a new position, specified by (x,y)
+function go(NewX, NewY) {
+  if (NewX == monster.posX && NewY == monster.posY) {
+    alert("Bonk!");
+  } else {
+    player.posX = NewX;
+    player.posY = NewY;
+  }
+  reDraw();
+}
+
+function fight() {
+  player.classList.add("bonk");
+  return 0;
 }
 
 // Initialization
